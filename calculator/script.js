c54820f748408c68d947c899dc9310b424d302bf
Shankerdev <shankerdev01@gmail.com>
2024-12-11 19:31:56 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,13 @@
+let screen = document.getElementById('screen')
+function btnClick(value){
+screen.value += value;
+}
+
+function clearscreen(){
+    screen.value="";
+}
+
+function findresult(){
+    var result = eval(screen.value)
+    screen.value = result;
+}
